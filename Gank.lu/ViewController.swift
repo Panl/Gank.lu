4f3ba94374e860a5896afe5e5f5ca910c8f3cd7a
潘磊 <panlei106@gmail.com>
2016-01-03 00:00:52 +0800

    init project

--- Gank.lu/ViewController.swift
+++ Gank.lu/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
