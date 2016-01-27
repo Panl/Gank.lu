f865cd6bad2646705288bcf156ce998efbdb31c7
潘磊 <panlei106@gmail.com>
2016-01-27 23:40:01 +0800

    nothing

--- Gank.lu/NextViewController.swift
+++ Gank.lu/NextViewController.swift
@@ -15,23 +15,11 @@ class NextViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         nextLabel.text = nextText
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
