b1fc6230a0f9434f9a3302784a2a090895536b8c
DianQK <xiaoqing@dianqk.org>
2016-02-28 10:17:08 +0800

    use appearance to setup UINavigationBar

--- Gank.lu/AppDelegate.swift
+++ Gank.lu/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let navBar = UINavigationBar.appearance()
+        navBar.barTintColor = UIColor(red: 44/255.0, green: 62/255.0, blue: 80/255.0, alpha: 1.0)
+        navBar.tintColor = UIColor.whiteColor()
+        navBar.barStyle = .BlackTranslucent
+        navBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
         return true
     }
 
