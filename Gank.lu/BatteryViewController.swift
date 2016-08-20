bc5b1f1d9a23cc2056ad2ccfe887901c87bc69da
Ray Pan <ray@glowing.com>
2016-08-20 10:38:25 +0800

    upgrade PagingMenuController

--- Gank.lu/BatteryViewController.swift
+++ Gank.lu/BatteryViewController.swift
@@ -9,28 +9,73 @@
 import UIKit
 import PagingMenuController
 
-class BatteryViewController: UIViewController {
+let tabTitles = ["Android","iOS","前端","瞎推荐","拓展资源","App"]
 
+private struct PagingMenuOptions : PagingMenuControllerCustomizable {
     
-    let tabTitles = ["Android","iOS","前端","瞎推荐","拓展资源","App"]
-    var viewControllers = [UIViewController]()
+    private var componentType: ComponentType {
+        return .All(menuOptions: MenuOptions(), pagingControllers: pagingControllers)
+    }
     
-    override func viewDidLoad(){
-        super.viewDidLoad()
+    private var pagingControllers: [UIViewController] {
+        var viewControllers = [UIViewController]()
         for title in tabTitles {
             let viewController = CategoryViewController(category: title)
             viewController.title = title
             viewControllers.append(viewController)
         }
-        let options = PagingMenuOptions()
-        options.menuHeight = 40
-        options.menuDisplayMode = .Standard(widthMode: .Flexible, centerItem: false, scrollingMode: .ScrollEnabled)
-        options.backgroundColor = navColor
-        options.selectedBackgroundColor = navColor
-        options.selectedTextColor = UIColor.whiteColor()
-        options.menuItemMode = .Underline(height: 3, color: UIColor.redColor(), horizontalPadding: 0, verticalPadding: 0)
-        let pagingMenuController = self.childViewControllers.first as! PagingMenuController
-        pagingMenuController.setup(viewControllers, options: options)
+        return viewControllers
+    }
+    
+    
+    private struct MenuOptions: MenuViewCustomizable {
+        var displayMode: MenuDisplayMode {
+            return .SegmentedControl
+        }
+        var backgroundColor: UIColor {
+            return navColor
+        }
+        var selectedBackgroundColor: UIColor {
+            return navColor
+        }
+        
+        var height: CGFloat {
+            return 40
+        }
+        var itemsOptions: [MenuItemViewCustomizable] {
+            var menuItems = [MenuItemViewCustomizable]()
+            for title in tabTitles {
+                menuItems.append(MenuItem(displayTitle: title))
+            }
+            return menuItems
+        }
+    }
+    
+    private struct MenuItem: MenuItemViewCustomizable {
+        var displayTitle = ""
+        init(displayTitle:String){
+            self.displayTitle = displayTitle
+        }
+        var displayMode: MenuItemDisplayMode {
+            return .Text(title: MenuItemText(text: displayTitle, selectedColor: UIColor.whiteColor()))
+        }
+    }
+}
+
+class BatteryViewController: UIViewController {
+
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        let options = PagingMenuOptions()        
+        let pagingMenuController = PagingMenuController(options: options)
+        pagingMenuController.view.frame.origin.y += 64
+        pagingMenuController.view.frame.size.height -= 64
+        
+        addChildViewController(pagingMenuController)
+        view.addSubview(pagingMenuController.view)
+        pagingMenuController.didMoveToParentViewController(self)
     }
     
     
